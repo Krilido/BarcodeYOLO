e5a5578a683fb8df91d1f050a8a76439069eab70
Raga Krilido <raga.krilido@gmail.com>
2015-07-23 16:18:43 +0700

    new

--- Android/IonicProject/www/js/app.js
+++ Android/IonicProject/www/js/app.js
@@ -15,40 +15,79 @@ var exampleApp = angular.module('starter', ['ionic','ngCordova'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+    if(window.Connection) {
+        if(navigator.connection.type == Connection.NONE) {
+            $ionicPopup.confirm({
+                title: "Internet Disconnected",
+                content: "The internet is disconnected on your device."
+            })
+        .then(function(result) {
+            if(!result) {
+                ionic.Platform.exitApp();
+            }
+        });
+        }
+    }
   });
 })
 
 
-/*
-$http.post("server/insert.php",{'fstname': $scope.newFriend.fname, 'lstname': $scope.newFriend.lname})
-        .success(function(data, status, headers, config){
-            console.log("inserted Successfully");
-        });
-*/
 exampleApp.controller("ExampleController", function($scope, $cordovaBarcodeScanner, $http) {
- 
+    var map = L.map('map');
+            
+/*    
+    $scope.lokasi = function(){
+        map.locate({setView: true, timeout: 10000, maxZoom: 16, enableHighAccuracy: true});
+            function onLocationError(e){
+              alert(e.message);
+            }
+            function onLocationFound(e) {
+                var radius = e.accuracy / 2;
+                // alert(e.latlng);
+                $scope.pos=e.latlng;
+                alert(e.latlng);
+      }
+      map.on('locationfound', onLocationFound);
+      map.on('locationerror', onLocationError);
+    }
+  });
+*/
     $scope.scanBarcode = function() {
         $cordovaBarcodeScanner.scan().then(function(imageData) {
             $scope.data=imageData.text;
-            
             // $scope.url='10.151.43.17/test/index.php';  
             // alert(imageData.text);
             // $http.post('10.151.43.17/test/index.php', data);
 
-                $http({
-                    method: 'POST',
-                    url: 'http://192.168.0.102/test/index.php',
-                    data: {'message': imageData.text}
-                })
-            // $http.post('http://10.151.43.101/test/index.php', {data})
-                .success(function(data, status, headers, config){
-                    alert("inserted Successfully");
-                }).
-                error(function(data, status, headers, config) {
-                    alert("inserted unSuccessfully");
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-                });
+
+            map.locate({setView: true, timeout: 10000, maxZoom: 16, enableHighAccuracy: true});
+            function onLocationError(e){
+              alert(e.message);
+            }
+            function onLocationFound(e) {
+                var radius = e.accuracy / 2;
+                // alert(e.latlng);
+                $scope.pos=e.latlng;
+                console.log(e.latlng)
+              $http({
+                  method: 'POST',
+                  url: 'http://192.168.0.107/test/index.php',
+                  data: {'lng': e.latlng.lng,'lat': e.latlng.lat,'message': imageData.text}
+              })
+          // $http.post('http://10.151.43.101/test/index.php', {data})
+              .success(function(data, status, headers, config){
+                  
+
+                  alert("inserted Successfully");
+              }).
+              error(function(data, status, headers, config) {
+                  alert("inserted unSuccessfully");
+              // called asynchronously if an error occurs
+              // or server returns response with an error status.
+              });
+            }
+            map.on('locationfound', onLocationFound);
+            map.on('locationerror', onLocationError);
             alert(imageData.text);
             // console.log("Barcode Format -> " + imageData.format);
             // console.log("Cancelled -> " + imageData.cancelled);
@@ -59,9 +98,76 @@ exampleApp.controller("ExampleController", function($scope, $cordovaBarcodeScann
  
 });
 
-
-
 /*
+
+var R = 6371; // km
+var dLat = (lat2-lat1).toRad();
+var dLon = (lon2-lon1).toRad();
+var lat1 = lat1.toRad();
+var lat2 = lat2.toRad();
+
+var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+var d = R * c;
+
+
+navigator.geolocation.getCurrentPosition(locationHandler);
+
+ function locationHandler(position)
+ {
+   var lat = position.coords.latitude;
+   var lng = position.coords.longitude;
+ }
+
+
+
+
+module.controller('GeoCtrl', function($cordovaGeolocation) {
+
+  var posOptions = {timeout: 10000, enableHighAccuracy: false};
+  $cordovaGeolocation
+    .getCurrentPosition(posOptions)
+    .then(function (position) {
+      var lat  = position.coords.latitude
+      var long = position.coords.longitude
+    }, function(err) {
+      // error
+    });
+
+
+  var watchOptions = {
+    frequency : 1000,
+    timeout : 3000,
+    enableHighAccuracy: false // may cause errors if true
+  };
+
+  var watch = $cordovaGeolocation.watchPosition(watchOptions);
+  watch.then(
+    null,
+    function(err) {
+      // error
+    },
+    function(position) {
+      var lat  = position.coords.latitude
+      var long = position.coords.longitude
+  });
+
+
+  watch.clearWatch();
+  // OR
+  $cordovaGeolocation.clearWatch(watch)
+    .then(function(result) {
+      // success
+      }, function (error) {
+      // error
+    });
+});
+
+
+
+
+
 exampleApp.controller("ExampleController",['$scope', '$http',  function(scope, $cordovaBarcodeScanner) {
     $scope.scanBarcode = function() {
       $cordovaBarcodeScanner.scan().then(function(imageData) {
